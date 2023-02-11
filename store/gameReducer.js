46a558b3e41f9d82aa473762822b0c10c6c7f61f
ali <websila.ir@gmail.com>
2023-02-11 21:24:57 +0330

    some debug

--- store/gameReducer.js
+++ store/gameReducer.js
@@ -7,24 +7,25 @@ export function gameReducer(state, action) {
     case "HEARTS_UP": {
       const newState = state;
       console.log("UP");
-      /* const playerId = action.payload.playerId;
-        const index = newState.attributes.player.findIndex((player) => {
-          return (player.id = playerId);
-        });
-        newState.attributes.player[index].hearts =
-          Number(newState.attributes.player[index].hearts) + 1; */
+      const playerId = action.payload.playerId;
+      const index = newState.attributes.player.findIndex((player) => {
+        return (player.id = playerId);
+      });
+      newState.attributes.player[index].hearts =
+        Number(newState.attributes.player[index].hearts) + 1;
+      console.log(newState.attributes.player[index]);
       return newState;
     }
     case "HEARTS_DOWN": {
       console.log("DOWN");
       const newState = state;
-      /* const playerId = action.payload.playerId;
-        const index = newState.attributes.player.findIndex((player) => {
-          return player.id === playerId;
-        });
-        newState.attributes.player[index].hearts =
-          Number(newState.attributes.player[index].hearts) - 1;
-        console.log(newState.attributes.player[index]); */
+      const playerId = action.payload.playerId;
+      const index = newState.attributes.player.findIndex((player) => {
+        return player.id === playerId;
+      });
+      newState.attributes.player[index].hearts =
+        Number(newState.attributes.player[index].hearts) - 1;
+      console.log(newState.attributes.player[index]);
       return newState;
     }
     default: {
