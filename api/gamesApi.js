605d58f51181c3ae0d3d7d8b3fd6a4b5ec3a2996
ali <websila.ir@gmail.com>
2023-02-24 21:02:01 +0330

    dynamic endpoint

--- api/gamesApi.js
+++ api/gamesApi.js
@@ -2,14 +2,14 @@ import axios from "axios";
 
 export async function getGames() {
   return await axios.get(
-    "http://localhost:1337/api/games?populate=*&sort[0]=createdAt:desc"
+    `${process.env.ENDPOINT_URL}/games?populate=*&sort[0]=createdAt:desc`
   );
 }
 
 export async function getGameById(id) {
   if (!id) return;
   return await axios.get(
-    `http://localhost:1337/api/games/${id}?populate[0]=player&populate[1]=player.avatar`
+    `${process.env.ENDPOINT_URL}/games/${id}?populate[0]=player&populate[1]=player.avatar`
   );
 }
 
@@ -20,7 +20,7 @@ export async function updatePlayers(gameId, players) {
     return player;
   });
   return await axios.put(
-    `http://localhost:1337/api/games/${gameId}?populate[0]=player&populate[1]=player.avatar`,
+    `${process.env.ENDPOINT_URL}/games/${gameId}?populate[0]=player&populate[1]=player.avatar`,
     {
       data: {
         player: [...newPlayers],
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env: {
+    ENDPOINT_URL: process.env.ENDPOINT_URL,
+  },
 };
 
 module.exports = nextConfig;
