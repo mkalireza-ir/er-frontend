ebbb7e2e4e38dcdcd1cb8d67e50b29427887a84c
ali <websila.ir@gmail.com>
2023-02-24 21:26:58 +0330

    pm2 added

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "next",
+      cwd: "/home/ubuntu/er-frontend-next12",
+      script: "npm",
+      args: "start",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
